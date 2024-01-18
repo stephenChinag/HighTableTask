8f2169866395edd357ef8eebf24a2fe7b426099e
StpehenChinag <stephenchinag@gmail.com>
2024-01-18 16:10:15 +0100

    signUp

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,11 +2,38 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { axios } from "axios";
+import { useState } from "react";
 
 export default function SignupPage() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSingUp = async () => {};
+
+  const userChangeHandler = () => {};
+  const emailChangeHandler = () => {};
+  const passwordChangeHandler = () => {};
   return (
-    <div className="flex">
-      <h1> Sign Up</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1> SignUp</h1>
+      <hr />
+      <label htmlFor="username">UserName</label>
+      <input
+        id="username"
+        type="text"
+        value={user.username}
+        onChange={userChangeHandler}
+      />
+      <label htmlFor="username">UserName</label>
+      <input
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={emailChangeHandler}
+      />
     </div>
   );
 }
