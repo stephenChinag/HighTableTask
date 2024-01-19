b93fd0242063fcd29e87d0f0cb29a5c41e69a481
StpehenChinag <stephenchinag@gmail.com>
2024-01-19 04:42:57 +0100

    profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import Navbar from "@/components/Navbar";
 
 export default function ProfilePage() {
   const router = useRouter();
@@ -19,12 +20,9 @@ export default function ProfilePage() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1> Profile Page </h1>
-      <hr />
-      <p> Profile Page</p>
+    <div className="flex flex-col gap-4 bg-gray-100 min-h-screen ">
+      <Navbar />
 
-      <hr />
       <button
         className="bg-blue-500 mt-4 hover:bg-blue-700 text-white front-bold py-2 px-4 rounded"
         onClick={onLogOuttHandler}
